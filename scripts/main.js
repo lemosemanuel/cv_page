6e99dfba5d8ba9fd038fd062dc7265d3a0f70b75
“lemosemanuel” <107789038+LemosEma@users.noreply.github.com>
2024-08-14 05:07:11 -0400

    prueba2

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('nav ul li a');
+// Buscador de habilidades
+document.getElementById('searchSkills').addEventListener('input', function() {
+    let searchQuery = this.value.toLowerCase();
+    let skills = document.querySelectorAll('.skill-category');
 
-    for (const link of navLinks) {
-        link.addEventListener('click', smoothScroll);
-    }
+    skills.forEach(skill => {
+        if (skill.textContent.toLowerCase().includes(searchQuery)) {
+            skill.style.display = '';
+        } else {
+            skill.style.display = 'none';
+        }
+    });
+});
 
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = event.currentTarget.getAttribute("href").substring(1);
-        const targetPosition = document.getElementById(targetId).offsetTop;
-        window.scrollTo({
-            top: targetPosition - 50,
-            behavior: "smooth"
-        });
-    }
+// Funcionalidad de acordeón para experiencias laborales
+let acc = document.querySelectorAll('.accordion');
+acc.forEach(button => {
+    button.addEventListener('click', function() {
+        this.classList.toggle('active');
+        let panel = this.nextElementSibling;
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+        } else {
+            panel.style.display = 'block';
+        }
+    });
 });
