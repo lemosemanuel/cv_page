01d765a99f8da752fa31aed16a36269659f9e6d8
“lemosemanuel” <107789038+LemosEma@users.noreply.github.com>
2024-08-14 23:26:30 -0400

    prueba2

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 document.querySelectorAll('.see-more').forEach(button => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
-        alert('Próximamente: más detalles sobre mi experiencia.');
+        const id = button.getAttribute('data-id');
+        const moreInfo = document.getElementById(id);
+        
+        if (moreInfo.style.display === "none") {
+            moreInfo.style.display = "block";
+            button.textContent = "Ver menos";
+        } else {
+            moreInfo.style.display = "none";
+            button.textContent = "Ver más";
+        }
     });
 });
