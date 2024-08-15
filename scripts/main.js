7bc615c61d12c2b45a7410e4bebdc26c95d88465
“lemosemanuel” <107789038+LemosEma@users.noreply.github.com>
2024-08-15 05:28:34 -0400

    prueba2

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
-// Manejador para mostrar/ocultar información adicional
 document.querySelectorAll('.see-more').forEach(button => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
         const id = button.getAttribute('data-id');
         const moreInfo = document.getElementById(id);
-
+        
         if (moreInfo.style.display === "none" || moreInfo.style.display === "") {
             moreInfo.style.display = "block";
             button.textContent = "Ver menos";
@@ -15,7 +14,6 @@ document.querySelectorAll('.see-more').forEach(button => {
     });
 });
 
-// Manejador para búsqueda y resaltado de habilidades
 document.getElementById('skill-search').addEventListener('input', function() {
     const query = this.value.toLowerCase();
     const skills = document.querySelectorAll('.skills-grid .skill');
@@ -45,37 +43,5 @@ document.getElementById('skill-search').addEventListener('input', function() {
     });
 });
 
-// Manejador para el cambio de idioma
-document.addEventListener('DOMContentLoaded', () => {
-    const languageSelect = document.getElementById('lang-switch');
 
-    const loadTranslations = async (lang) => {
-        const response = await fetch('translations.json');
-        const translations = await response.json();
-        return translations[lang];
-    };
 
-    const changeLanguage = async (lang) => {
-        const translations = await loadTranslations(lang);
-        document.querySelectorAll('[data-translate]').forEach((element) => {
-            const key = element.getAttribute('data-translate');
-            if (translations[key]) {
-                element.textContent = translations[key];
-            }
-        });
-
-        // Cambiar el título de la página
-        document.title = translations['page-title'];
-    };
-
-    languageSelect.addEventListener('click', (event) => {
-        const currentLang = event.target.getAttribute('data-lang');
-        const newLang = currentLang === 'es' ? 'en' : 'es';
-        event.target.setAttribute('data-lang', newLang);
-        event.target.textContent = newLang.toUpperCase();
-        changeLanguage(newLang);
-    });
-
-    // Establecer el idioma inicial
-    changeLanguage('es');
-});
